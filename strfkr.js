8d61b6bc62accd6fc6cc1e5b711b33775a5b8284
Paras Sanghavi <sanghaviparas@gmail.com>
2017-02-11 20:49:53 -0800

    eslint

--- strfkr.js
+++ strfkr.js
@@ -8,29 +8,29 @@ function setup() {
 }
 
 function draw() {
-        numTriangles = windowHeight/scale	
-	for(var i=0; i<numTriangles; i++) {
-		fill(randomColor())
+    numTriangles = windowHeight/scale
+    for(var i=0; i<numTriangles; i++) {
+        fill(randomColor())
          	triangle(
 			0, windowHeight/2,
 			windowWidth/2, i*scale,
 			windowWidth/2, i*scale + scale
 		)
-	}
-	for(var i=0; i<numTriangles; i++) {
-		fill(randomColor())
+    }
+    for(var i=0; i<numTriangles; i++) {
+        fill(randomColor())
          	triangle(
 			windowWidth/2, i*scale,
 			windowWidth/2, i*scale + scale,
 			windowWidth, windowHeight/2
 		)
-	}
+    }
 }
 
 function randomColor() {
-	r = random(255)
-        g = random(255)
-        b = random(255)
+    r = random(255)
+    g = random(255)
+    b = random(255)
 
-	return color(r,g,b)
+    return color(r,g,b)
 }
